54a4cb8c756b8178a3548040df2d670b48ea2514
Devendra Soni <devendrasoni984@gmail.com>
2022-12-24 19:41:46 +0530

    bank finder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-
-dfgsdfgdfsgdfs
-
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ifscCode: null,
+      BankData: null,
+    };
+    
+  }
+  setIfscCode = (e) => {
+    
+    this.setState({
+      ifscCode: e && e.target && e.target.value,
+    });
+  };
+  GetDetail(value) {
+    debugger
+    if (value && value.length > 0) {
+      fetch(`https://ifsc.razorpay.com/${value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          this.setState({
+            BankData: data,
+          });
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    } else {
+      alert("Please eneter valid detai;ls");
+    }
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <input value={this.state.ifscCode} onChange={this.setIfscCode.bind()} />
+        </div>
+        <button onClick={()=>{this.GetDetail(this.state.ifscCode)}}>GetDeyail </button>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
